efad4e4c99be9e5d2dc32906deb371d2ab59acd1
Light Ng <shadowmydx@126.com>
2018-08-09 16:28:15 +0800

    add test for load real data from memory

--- DetectBug.py
+++ DetectBug.py
@@ -1,6 +1,7 @@
 from DataStructure import *
 from MainProcess import construct_memory_execute_mode
 from MainProcess import parse_target_memory_and_checking_sync
+import random
 
 
 def count_star(target_str):
@@ -142,7 +143,7 @@ def test_copy_from_mat():
 
 def test_trace_mat_mat_trans():
     test_block = Block((-1, -1, 0), (1, 1, 1))
-    test_thread = Thread((-1, -1, 0), (3, 1, 1))
+    test_thread = Thread((-1, -1, 0), (40, 1, 1))
     global_current_env = parse_function("./kaldi-new-bug/new-func.ll")
     arguments = generate_arguments(global_current_env.get_value("@_Z20_trace_mat_mat_transPKfS0_iiiiPf"), {
         "%dA_rows": 5,
@@ -162,13 +163,51 @@ def test_trace_mat_mat_trans():
                                   global_current_env, False)
 
 
+def generate_random_data_for_memory(target_memory_container, target_memory, total_size):
+    low_bound = 1
+    high_bound = 100
+    for index in xrange(total_size):
+        memory_shell = DataType("..")
+        memory_shell.memory_index = index
+        memory_shell.set_value(target_memory)
+        current_data = DataType("i32")
+        current_data.set_value(random.randint(low_bound, high_bound))
+        target_memory_container.add_value_to_memory(memory_shell, current_data)
+
+
+def test_slice():
+    test_block = Block((-1, -1, 0), (1, 1, 1))
+    test_thread = Thread((-1, -1, 0), (3, 2, 1))
+    global_current_env = parse_function("./kaldi-new-bug/new-func.ll")
+    arguments = generate_arguments(global_current_env.get_value("@_Z7_splicePfPKfPKiiiiiii"), {
+        "%d_out_cols": 5,
+        "%d_out_rows": 5,
+        "%d_out_stride": 1,
+        "%d_in_cols": 5,
+        "%d_in_rows": 5,
+        "%d_in_stride": 1,
+    })
+    arguments["main_memory"] = {
+        'global': "%y",
+        'shared': None,
+    }
+    generate_memory_container(["%off"], global_current_env)
+    target_memory = global_current_env.get_value("memory_container")
+    generate_random_data_for_memory(target_memory, "%off", 100)
+    raw_code = global_current_env.get_value("@_Z7_splicePfPKfPKiiiiiii")
+    construct_memory_execute_mode(test_block, test_thread, 100, 256, raw_code.raw_codes, arguments,
+                                  parse_target_memory_and_checking_sync, parse_target_memory_and_checking_sync,
+                                  global_current_env, False)
+
+
 if __name__ == "__main__":
     # global_test_env = parse_function("./kaldi-new-bug/new-func.ll")
     # test_copy_low_upp()
     # test_copy_upp_low()
     # test_add_diag_vec_mat()
-    # test_copy_from_tp()
+    test_copy_from_tp()
     # test_copy_from_mat()
-    test_trace_mat_mat_trans()
+    # test_trace_mat_mat_trans()
+    # test_slice()
     print 'over'
 
